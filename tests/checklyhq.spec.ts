fb1cfcbfeb80fa6ee64058c431661cdd233f7b13
Laura Guo <lauraguo@proton.me>
2025-01-20 15:58:49 -0500

    Create Playwright tests

---
+++ tests/checklyhq.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test('navigate to docs and search for traces', async ({ page }) => {
+  await page.goto('https://www.checklyhq.com/');
+  await page.getByRole('button', { name: 'Developers' }).first().click();
+  await page.getByRole('link', { name: 'Documentation Technical docs' }).click();
+  await expect(page.getByRole('heading', { name: 'Getting started with Checkly' })).toBeVisible();
+  await page.getByLabel('Search (Command+K)').click();
+  await expect(page.getByPlaceholder('Search docs')).toBeEmpty();
+  await page.getByPlaceholder('Search docs').click();
+  await page.getByPlaceholder('Search docs').fill('traces');
+  await page.getByRole('link', { name: 'Importing your traces to' }).click();
+  await expect(page.getByRole('heading', { name: 'Importing your traces to' })).toBeVisible();
+});
+
+test('navigate to main blog it loads correctly', async ({ page }) => {
+  await page.goto('https://www.checklyhq.com');
+  await page.getByRole('link', { name: 'Blog' }).first().click();
+  await expect(page.getByRole('heading', { name: 'The Checkly Blog' })).toBeVisible();
+
+  const post = page.locator('.section-blog').first();
+  const postName = await post.locator('h2').textContent();
+  await post.click();
+  
+  if(postName) {
+    await expect(page.locator('h1')).toContainText(postName);
+  } else {
+    test.fail();
+  }
+});
